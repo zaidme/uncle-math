1c21a7dd52ab3f7bc2a92ee39bb742d603e0f008
zaid42005 <zaid.42005@gmail.com>
2024-11-08 22:37:31 -0500

    pango

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install system dependencies
-apk add --no-cache libpango-dev pkg-config python3-dev
+apk add --no-cache libpango1.0-dev pkg-config python3-dev
 
 # Install Python dependencies
 python3 -m pip install --no-cache-dir -r requirements.txt
